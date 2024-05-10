3575185b5af09639e0f0f8f9c6ec538828b8c639
Jeong Chong In <chongin12@naver.com>
2024-05-10 11:50:41 +0900

    Initial Commit

---
+++ LabDuck/LabDuck/LabDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabDuckApp.swift
+//  LabDuck
+//
+//  Created by 정종인 on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct LabDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
