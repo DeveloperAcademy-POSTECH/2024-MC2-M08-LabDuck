369e1e184c40daf68548ed1a5389179501154b3a
seoyounghan <happyunbd364@naver.com>
2024-05-13 20:20:50 +0900

    [#16] 테이블에서 더미 데이터 띄우기

--- LabDuck/View/TableView.swift
+++ LabDuck/View/TableView.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct TableView: View {
     @State private var nodes: [KPNode] = .mockData
+    @State private var selection = Set<KPNode.ID>()
+    
     var body: some View {
         VStack{
-            Table(of: KPNode.self) {
+            Table(of: KPNode.self, selection: $selection) {
+                TableColumn("색") { node in
+                    Circle()
+                        .frame(width: 20, height: 20)
+                        .foregroundColor(node.colorTheme.backgroundColor)
+                        
+                }
+                .width(20)
+                
                 TableColumn("제목") { node in
-                    
                     Text(node.title ?? " ")
+                }
+                
+                TableColumn("태그") { node in
+                    HStack {
+                        ForEach(node.tags) { tag in
+                            Text("#\(tag.name)")
+                        }
+                    }
+                }
+                
+                TableColumn("링크") { node in
+                    Link(destination: URL(string: node.url ?? " ")!, label: {
+                        Text(node.url ?? " ")
+                    })
+                }
+                
+                TableColumn("노트") { node in
+                    Text(node.note ?? " ")
                 }
                 
             } rows: {
@@ -23,6 +50,9 @@ struct TableView: View {
                 }
             }
         }
+        .tableStyle(.inset(alternatesRowBackgrounds: false))
+                   .scrollContentBackground(.hidden)
+                   .background(RadialGradient(colors:[.white,.white], center:UnitPoint(x:1,y:1), startRadius: 0.0, endRadius:20))
     }
 }
 
