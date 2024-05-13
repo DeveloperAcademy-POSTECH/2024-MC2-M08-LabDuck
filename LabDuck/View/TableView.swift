00c892004748432cd2c278ccc6247c8fb9b85cae
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2024-05-14 08:53:47 +0900

    [#16] 테이블 뷰 더미 데이터 띄워서 만들기 (#20)
    * [#16] 더미데이터에 title, note, url, tags 정보 추가
    
    * [#16] 테이블에서 더미 데이터 띄우기
    
    * [#16]TableView - background 제거, Circle의 frame 제거

---
+++ LabDuck/View/TableView.swift
@@ -0,0 +1,60 @@
+//
+//  TableView.swift
+//  LabDuck
+//
+//  Created by hanseoyoung on 5/13/24.
+//
+
+import SwiftUI
+
+struct TableView: View {
+    @State private var nodes: [KPNode] = .mockData
+    @State private var selection = Set<KPNode.ID>()
+    
+    var body: some View {
+        VStack{
+            Table(of: KPNode.self, selection: $selection) {
+                TableColumn("색") { node in
+                    Circle()
+                        .foregroundColor(node.colorTheme.backgroundColor)
+                    
+                }
+                .width(20)
+                
+                TableColumn("제목") { node in
+                    Text(node.title ?? " ")
+                }
+                
+                TableColumn("태그") { node in
+                    HStack {
+                        ForEach(node.tags) { tag in
+                            Text("#\(tag.name)")
+                        }
+                    }
+                }
+                
+                TableColumn("링크") { node in
+                    Link(destination: URL(string: node.url ?? " ")!, label: {
+                        Text(node.url ?? " ")
+                    })
+                }
+                
+                TableColumn("노트") { node in
+                    Text(node.note ?? " ")
+                }
+                
+            } rows: {
+                ForEach(nodes) { node in
+                    TableRow(node)
+                }
+            }
+        }
+        .tableStyle(.inset(alternatesRowBackgrounds: false))
+        .scrollContentBackground(.hidden)
+    }
+}
+
+
+#Preview {
+    TableView()
+}
