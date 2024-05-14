55f978c5d110c38bb0b55ee4a7dbcd48edf0cf84
seoyounghan <happyunbd364@naver.com>
2024-05-14 14:38:07 +0900

    [#21] TableView 정렬 기능 추가

--- LabDuck/View/TableView.swift
+++ LabDuck/View/TableView.swift
@@ -10,19 +10,21 @@ import SwiftUI
 struct TableView: View {
     @State private var nodes: [KPNode] = .mockData
     @State private var selection = Set<KPNode.ID>()
+    @State private var sortOrder = [KeyPathComparator(\KPNode.title)]
     
     var body: some View {
         VStack{
-            Table(of: KPNode.self, selection: $selection) {
+            Table(nodes, selection: $selection, sortOrder: $sortOrder) {
                 TableColumn("색") { node in
                     Circle()
                         .foregroundColor(node.colorTheme.backgroundColor)
-                    
                 }
                 .width(20)
                 
-                TableColumn("제목") { node in
-                    Text(node.title ?? " ")
+                TableColumn("제목", value: \.unwrappedTitle) { node in
+                    Text(node.unwrappedTitle)
+                        .lineLimit(1)
+                        .padding(10)
                 }
                 
                 TableColumn("태그") { node in
@@ -33,26 +35,29 @@ struct TableView: View {
                     }
                 }
                 
-                TableColumn("링크") { node in
+                TableColumn("링크", value: \.unwrappedURL) { node in
                     Link(destination: URL(string: node.url ?? " ")!, label: {
-                        Text(node.url ?? " ")
+                        Text(node.unwrappedURL)
+                            .lineLimit(1)
                     })
                 }
                 
-                TableColumn("노트") { node in
-                    Text(node.note ?? " ")
+                TableColumn("노트", value: \.unwrappedNote) { node in
+                    Text(node.unwrappedNote)
                 }
                 
-            } rows: {
-                ForEach(nodes) { node in
-                    TableRow(node)
-                }
             }
+            .tableStyle(.inset(alternatesRowBackgrounds: false))
+            .scrollContentBackground(.hidden)
+            .onChange(of: sortOrder) { newOrder in
+                nodes.sort(using: newOrder)
+            }
+        
+            
         }
-        .tableStyle(.inset(alternatesRowBackgrounds: false))
-        .scrollContentBackground(.hidden)
     }
 }
+
 
 
 #Preview {
