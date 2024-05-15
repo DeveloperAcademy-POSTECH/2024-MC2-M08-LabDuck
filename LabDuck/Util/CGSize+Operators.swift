ee344a97b7d3e586e3e76a81527ab96b49b84bbf
heracloud <heracloud7@naver.com>
2024-05-15 14:09:00 +0900

    [#27] 지속적으로 축소 시 프로그램이 종료되는 현상 해결

--- LabDuck/Util/CGSize+Operators.swift
+++ LabDuck/Util/CGSize+Operators.swift
@@ -10,7 +10,7 @@ import Foundation
 extension CGSize {
     static func + (lhs: Self, rhs: Self) -> Self {
         Self(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
-    }//두개의 cgsize 받아서 각 구조체의 너비와 높이를 더한 새로운 cgsize 반환
+    }
     static func += (lhs: inout Self, rhs: Self) {
         lhs = lhs + rhs
     }
