e8a4755f4befa114b839105c049db023a9281e17
seoyounghan <63441374+seoyounghan@users.noreply.github.com>
2024-05-15 23:29:39 +0900

    [#25] TableView에서 output 표현 (#31)
    * [#25] KPTag 구조에 colorTheme 추가
    
    * [#25] TableView에 outputPoint를 하위 테이블로 표시해 주는 기능 추가
    
    * [#25] findNodes 로직 수정
    
    * [#25] forEach로 구문 변경

--- LabDuck/Model/KPTag.swift
+++ LabDuck/Model/KPTag.swift
@@ -6,14 +6,22 @@
 //
 
 import Foundation
+import SwiftUI
 
 struct KPTag: Identifiable {
     var id: UUID
     var name: String
+    var colorTheme: Color
 }
 
 extension KPTag {
     static var mockData: KPTag {
-        .init(id: UUID(), name: "tag1")
+        .init(id: UUID(), name: "tag1", colorTheme: Color.blue)
+    }
+    static var mockData1: KPTag {
+        .init(id: UUID(), name: "tag2", colorTheme: Color.green)
+    }
+    static var mockData2: KPTag {
+        .init(id: UUID(), name: "tag3", colorTheme: Color.yellow)
     }
 }
--- LabDuck/View/TableView.swift
+++ LabDuck/View/TableView.swift
@@ -8,18 +8,24 @@
 import SwiftUI
 
 struct TableView: View {
-    @State private var nodes: [KPNode] = .mockData
+    @State var nodes: [KPNode] = .mockData
+    @State private var expanded: Bool = true
     @State private var selection = Set<KPNode.ID>()
     @State private var sortOrder = [KeyPathComparator(\KPNode.title)]
+    //Edge 더미데이터 -> 표에서 output을 표현하기 위함
+    @State var edges: [KPEdge] = [
+        KPEdge(sourceID: Array.mockData[0].outputPoints[0].id, sinkID: Array.mockData[1].inputPoints[0].id),
+        KPEdge(sourceID: Array.mockData[0].outputPoints[0].id, sinkID: Array.mockData[3].inputPoints[0].id),
+    ]
     
     var body: some View {
         VStack{
-            Table(nodes, selection: $selection, sortOrder: $sortOrder) {
+            Table(of: KPNode.self, selection: $selection, sortOrder: $sortOrder) {
                 TableColumn("색", value: \.colorTheme.rawValue) { node in
                     Circle()
                         .foregroundColor(node.colorTheme.backgroundColor)
                 }
-                .width(20)
+                .width(30)
                 
                 TableColumn("제목", value: \.unwrappedTitle) { node in
                     Text(node.unwrappedTitle)
@@ -30,7 +36,15 @@ struct TableView: View {
                 TableColumn("태그") { node in
                     HStack {
                         ForEach(node.tags) { tag in
-                            Text("#\(tag.name)")
+                            Button(action: {
+                                // 추후 tag를 눌렀을 때 기능 추가 가능하도록
+                            }) {
+                                Text("#\(tag.name)")
+                                    .foregroundColor(.white)
+                                    .padding(5)
+                                    .background(tag.colorTheme)
+                                    .cornerRadius(40)
+                            }
                         }
                     }
                 }
@@ -46,19 +60,43 @@ struct TableView: View {
                     Text(node.unwrappedNote)
                 }
                 
+            } rows: {
+                ForEach(nodes, id: \.id) { node in
+                    if findNodes(matching: node).isEmpty {
+                        TableRow(node)
+                    } else {
+                        DisclosureTableRow(node) {
+                            ForEach(findNodes(matching: node))
+                        }
+                    }
+                }
             }
             .tableStyle(.inset(alternatesRowBackgrounds: false))
             .scrollContentBackground(.hidden)
             .onChange(of: sortOrder) { newOrder in
-                nodes.sort(using: newOrder)
-            }
-        
-            
+                nodes.sort(using: newOrder) }
         }
     }
+    // MARK: - 노드의 ouputPoint에 대한 inputPoint들을 찾아 해당 노드들 리턴
+    func findNodes(matching node: KPNode) -> [KPNode] {
+        let sourceIDs = node.outputPoints.map { $0.id }
+        
+        let matchingSinkIDs = edges.filter { sourceIDs.contains($0.sourceID) }.map { $0.sinkID }
+        
+        var nodeDict = [UUID: KPNode]()
+        
+        nodes.forEach { node in
+            node.inputPoints.forEach { inputPoint in
+                if matchingSinkIDs.contains(inputPoint.id) {
+                    nodeDict[node.id] = node
+                }
+            }
+        }
+        
+        return Array(nodeDict.values)
+    }
+    
 }
-
-
 
 #Preview {
     TableView()
