ebc62c8ccaeabbfc5b0dc47fc501a51ba486ae14
Hajin Lee <springlhj2@naver.com>
2024-05-18 14:33:33 +0900

    [#41] 노드 꾸미기 뷰 수정

--- LabDuck/View/DetailNodeView.swift
+++ LabDuck/View/DetailNodeView.swift
@@ -13,133 +13,147 @@ struct DetailNodeView: View {
     @State var hovered: Bool = false
     @State private var isEditing: Bool = false
     
-    //더미 데이터
-    @State private var inputTag: String = "#tag1"
-    @State private var title: String = "Cracking the code: Co-coding with AI in creative programming education"
-    @State private var note: String = "Related work 살펴보기 좋을 듯 함. 근데 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구 어쩌구 저쩌구"
-    @State private var link: String = "https://dl.acm.org/doi/abs/10.1145/3527927.3532801"
+    @State private var nodes: [KPNode] = .mockData
+    @State var kptag: [KPTag] = []
     
-    
-    @State var kptag:[KPTag] = []
-//    @State var kpinputpoint:[KPInputPoint] = []
-//    @State var kpoutputpoint:[KPOutputPoint] = []
+    //    @State var kpinputpoint:[KPInputPoint] = []
+    //    @State var kpoutputpoint:[KPOutputPoint] = []
     
     
     
     
     var body: some View {
         ZStack{
-            VStack(spacing: 10) {
-                HStack{
-                    VStack{
-                        
-                        //나중에 input/output추가할 때 쓸 코드
-                        
-                        //                        ForEach(kpinputpoint) { point in
-                        //                            Image(systemName: "arrow.right.circle.fill").imageScale(.large).padding(EdgeInsets(top: 20, leading: 8, bottom: 20, trailing: 8))
-                        //                        }
-                        //
-                        //                        Button{kpinputpoint.append(KPInputPoint(id: UUID(), name: inputTag))}label: {
-                        //                            Text("+")
-                        //                                .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: 8))
-                        //                                .background(Color.gray).opacity(0.8)
-                        //                                .cornerRadius(20)
-                        //                        }.buttonStyle(BorderlessButtonStyle())
-                        //                            .background(Color.clear) // 배경을 투명하게 설정
-                        //                            .overlay(Rectangle().stroke(Color.clear))
-                        Image(systemName: "arrow.right.circle.fill").imageScale(.large).padding(EdgeInsets(top: 20, leading: 8, bottom: 20, trailing: 8))
-                        
-                        Image(systemName: "arrow.right.circle.fill").imageScale(.large).padding(EdgeInsets(top: 20, leading: 8, bottom: 20, trailing: 8))
-                        
-                        Image(systemName: "arrow.right.circle.fill").imageScale(.large).padding(EdgeInsets(top: 20, leading: 8, bottom: 20, trailing: 8))
-                        
-                    }
+            
+            HStack(spacing: 20){
+                //좌측 아웃풋 포인트
+                VStack(spacing: 70){
+                    NodePoint()
+                    NodePoint()
+                    NodePoint()
+                    
+                    //나중에 input/output추가할 때 쓸 코드
+                    
+                    //                        ForEach(kpinputpoint) { point in
+                    //                            NodePoint()
+                    //                        }
+                    //
+                    //                        Button{kpinputpoint.append(KPInputPoint(id: UUID(), name: inputTag))}label: {
+                    //                            Text("+")
+                    //                                .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: 8))
+                    //                                .background(Color.gray).opacity(0.8)
+                    //                                .cornerRadius(20)
+                    //                        }.buttonStyle(BorderlessButtonStyle())
+                    //                            .background(Color.clear) // 배경을 투명하게 설정
+                    //                            .overlay(Rectangle().stroke(Color.clear)
+                    
+                }
+                ZStack{
+                    
                     Rectangle()
                         .frame(width: 250, height: 350)
-                        .foregroundColor(Color.white)
-                        .overlay(
-                            VStack(alignment: .leading, spacing: 10) {
-                                ScrollView(.horizontal) {
-                                    HStack{
-                                        
-                                        ForEach(kptag) { tag in
-                                            Text(inputTag)
-                                                .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: 8))
-                                                .background(Color.blue)
-                                                .cornerRadius(20)
-                                        }
-                                        
-                                        
-                                        //태그 추가 버튼
-                                        Button{kptag.append(KPTag(id: UUID(), name: inputTag))}label: {
-                                            Text("+")
-                                                .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: 8))
-                                                .background(Color.gray).opacity(0.8)
-                                                .cornerRadius(20)
-                                        }.buttonStyle(BorderlessButtonStyle())
-                                            .background(Color.clear) // 배경을 투명하게 설정
-                                            .overlay(Rectangle().stroke(Color.clear))
+                        .foregroundColor(Color.white).cornerRadius(10)
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        ScrollView(.horizontal) {
+                            
+                            
+                            HStack{
+                                
+                                ForEach(nodes[0].tags){ tag in
+                                    Text("#\(tag.name)").foregroundColor(.white)
+                                            .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: 8))
+                                            .background(Color.blue)
+                                            .cornerRadius(20)
                                     }
-                                }
-                                
-                                //제목
-                                if isEditing {
-                                    TextEditor(text: $title).scrollContentBackground(.hidden).background(Color.clear).border(Color.clear, width: 0).foregroundColor(.black).font(.system(size: 20, weight: .bold)).frame(width:200,height: 80)
-                                    
-                                } else {
-                                    Text(title).foregroundColor(.black)
-                                        .font(.system(size: 20, weight: .bold)).frame(width:200,height: 80)
-                                }
-                                
-                                Divider().background(.gray).padding(2)
-                                
-                                //노트
-                                Text("Note").foregroundColor(.black)
-                                
-                                if isEditing {
-                                    TextEditor(text: $note).scrollContentBackground(.hidden).background(Color.clear).border(Color.clear, width: 0).foregroundColor(.black).font(.system(size: 13)).frame(width:200,height: 40)
-                                } else {
-                                    Text(note).foregroundColor(.black)
-                                        .font(.system(size: 13, weight: .regular)).frame(width:200,height: 40)
-                                }
-                                
-                                Divider().background(.gray).padding(2)
-                                
-                                //링크
-                                Text("Link").foregroundColor(.black)
-                                
-                                
-                                if isEditing {
-                                    TextEditor(text: $link).scrollContentBackground(.hidden).background(Color.clear).border(Color.clear, width: 0).foregroundColor(.blue).underline().font(.system(size: 13))
-                                        .textFieldStyle(RoundedBorderTextFieldStyle()).frame(width:200,height: 40)
-                                } else {
-                                    Link(destination: URL(string: link)!, label: {
-                                        Text(link).foregroundColor(.blue).underline()
-                                            .font(.system(size: 13, weight: .regular))
-                                    }).frame(width:200,height: 40)
-                                    
-                                }
+                                    //태그 추가 버튼
+                                    Button{
+                                        nodes[0].tags.append(.mockData)
+                                    }label: {
+                                        Text("+")
+                                            .padding(EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: 8))
+                                            .background(Color.gray).opacity(0.8)
+                                            .cornerRadius(20)
+                                    }.buttonStyle(BorderlessButtonStyle())
                             }
-                                .padding(20)
-                        )
-                        .cornerRadius(10)
-                    VStack{
-                        Image(systemName: "arrow.right.circle.fill").imageScale(.large).padding(EdgeInsets(top: 20, leading: 8, bottom: 20, trailing: 8))
+                            
+                        }
                         
-                    }
+                        
+                        //제목
+                        if isEditing {
+                            
+                            TextEditor(text: $nodes[0].unwrappedTitle)
+                                .scrollContentBackground(.hidden).background(Color.clear).border(Color.clear, width: 0).foregroundColor(.black).font(.system(size: 20, weight: .bold)).frame(width:200,height: 80)
+                            
+                        } else {
+                            
+                            Text(nodes[0].unwrappedTitle).foregroundColor(.black)
+                                .font(.system(size: 20, weight: .bold)).frame(width:200,height: 80)
+                        }
+                        
+                        Divider().background(.gray).padding(2)
+                        
+                        //노트
+                        Text("Note").foregroundColor(.black)
+                        
+                        if isEditing {
+                            
+                            TextEditor(text: $nodes[0].unwrappedNote)
+                                .scrollContentBackground(.hidden).background(Color.clear).border(Color.clear, width: 0).foregroundColor(.black).font(.system(size: 13)).frame(width:200,height: 40)
+                            
+                        } else {
+                            
+                            Text(nodes[0].unwrappedNote).foregroundColor(.black)
+                                .font(.system(size: 13, weight: .regular)).frame(width:200,height: 40)
+                        }
+                        
+                        Divider().background(.gray).padding(2)
+                        
+                        //링크
+                        Text("Link").foregroundColor(.black)
+                        
+                        
+                        if isEditing {
+                            TextEditor(text: $nodes[0].unwrappedURL).scrollContentBackground(.hidden).background(Color.clear).border(Color.clear, width: 0).foregroundColor(.blue).underline().font(.system(size: 13)).frame(width:200,height: 40)
+                            
+                        } else {
+                            
+                            Link(destination: URL(string: nodes[0].unwrappedURL)!, label: {
+                                Text("\(nodes[0].unwrappedURL)").foregroundColor(.blue).underline()
+                                    .font(.system(size: 13, weight: .regular))
+                            }).frame(width:200,height: 40)
+                        }
+                        
+                        
+                        
+                    }.frame(width: 200, height: 350).padding(20)
+                    
+                    
                 }
-            }.padding()
+                .cornerRadius(10)
+                
+                //우측 인풋 포인트
+                VStack{
+                    NodePoint()
+                }
+            }
             
             //수정 버튼
-            Button{isEditing.toggle()}label:{
+            Button{
+                isEditing.toggle()
+            }label:{
                 Image(systemName: "square.and.pencil").foregroundColor(.gray).opacity(self.hovered ? 1.0 : 0.3).onHover { hover in
                     print("Mouse hover: \(hover)")
-                    self.hovered.toggle()
+                    self.hovered = hover
                 }
-            }.buttonStyle(BorderlessButtonStyle())
-                .background(Color.clear) // 배경을 투명하게 설정
-                .overlay(Rectangle().stroke(Color.clear)).offset(x:100,y:-110)
+            }.buttonStyle(BorderlessButtonStyle()).offset(x:100,y:-110)
         }
+    }
+    @ViewBuilder
+    private func NodePoint() -> some View {
+        Image(systemName: "arrow.right.circle.fill")
+            .imageScale(.large)
         
     }
 }
