94a69b601bc629f3c92242d61259a4409cb2f559
seoyounghan <happyunbd364@naver.com>
2024-05-18 21:14:08 +0900

    [#45] 노드 컬러, 태그 컬러 추가, 테이블뷰 순서 변경

--- LabDuck/Model/KPColorTheme.swift
+++ LabDuck/Model/KPColorTheme.swift
@@ -9,21 +9,37 @@ import SwiftUI
 
 enum KPColorTheme: Int {
     case `default`
-    case cyan
+    case red
+    case orange
+    case yellow
+    case green
+    case blue
+    case lavender
+    case purple
 
     var backgroundColor: Color {
         switch self {
         case .default:
             return Color.white
-        case .cyan:
-            return Color.blue
+        case .red:
+            return Color(hex: 0xFFC4C1)
+        case .orange:
+            return Color(hex: 0xFFDFB2)
+        case .yellow:
+            return Color(hex: 0xFFF0B2)
+        case .green:
+            return Color(hex: 0xBEF0C6)
+        case .blue:
+            return Color(hex: 0xB2D7FF)
+        case .lavender:
+            return Color(hex: 0xE7CBF5)
+        case .purple:
+            return Color(hex: 0xCDCCF3)
         }
     }
     var foregroundColor: Color {
         switch self {
-        case .default:
-            return Color.black
-        case .cyan:
+        case .default, .red, .orange, .yellow, .green, .blue, .lavender, .purple:
             return Color.black
         }
     }
--- LabDuck/Model/KPTag.swift
+++ LabDuck/Model/KPTag.swift
@@ -11,17 +11,17 @@ import SwiftUI
 struct KPTag: Identifiable {
     var id: UUID
     var name: String
-    var colorTheme: Color
+    var colorTheme: KPTagColor = KPTagColor.default
 }
 
 extension KPTag {
     static var mockData: KPTag {
-        .init(id: UUID(), name: "tag1", colorTheme: Color.blue)
+        .init(id: UUID(), name: "tag1", colorTheme: KPTagColor.blue)
     }
     static var mockData1: KPTag {
-        .init(id: UUID(), name: "tag2", colorTheme: Color.green)
+        .init(id: UUID(), name: "tag2", colorTheme: KPTagColor.red)
     }
     static var mockData2: KPTag {
-        .init(id: UUID(), name: "tag3", colorTheme: Color.yellow)
+        .init(id: UUID(), name: "tag3", colorTheme: KPTagColor.yellow)
     }
 }
