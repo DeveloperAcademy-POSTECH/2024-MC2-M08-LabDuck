99efd452acac79c89a0f867fe4717f6c106dadda
heracloud <heracloud7@naver.com>
2024-05-25 16:06:01 +0900

    [#77] merge 작업 진행

--- LabDuck/View/BoardGalleryView.swift
+++ LabDuck/View/BoardGalleryView.swift
@@ -10,9 +10,9 @@ struct KPBoardPreview: Identifiable {
 
 struct BoardGalleryView: View {
     @Environment(\.newDocument) private var newDocument
-
+    
     @State private var previews: [KPBoardPreview] = []
-
+    
     @State private var showAlert = false
     @State private var selectedBoard: KPBoardPreview?
     @State private var editingBoardID: UUID?
@@ -22,7 +22,7 @@ struct BoardGalleryView: View {
     let columns = [
         GridItem(.adaptive(minimum: 240), spacing: 10)
     ]
-
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 20) {
@@ -43,36 +43,36 @@ struct BoardGalleryView: View {
                                     }
                                 }
                             ))
-                                .onTapGesture(count: 2) {
+                            .onTapGesture(count: 2) {
+                                Task {
+                                    await openDocumentOnMainThread(preview.url.wrappedValue)
+                                }
+                            }
+                            .contextMenu(ContextMenu(menuItems: {
+                                Button(action: {
                                     Task {
                                         await openDocumentOnMainThread(preview.url.wrappedValue)
                                     }
+                                }) {
+                                    Text("파일 보기")
                                 }
-                                .contextMenu(ContextMenu(menuItems: {
-                                    Button(action: {
-                                        Task {
-                                            await openDocumentOnMainThread(preview.url.wrappedValue)
-                                        }
-                                    }) {
-                                        Text("파일 보기")
-                                    }
-                                    Button(action: {
-                                        editingBoardID = preview.id
-                                    }) {
-                                        Text("이름 바꾸기")
-                                    }
-                                    Button(action: {
-//                                        duplicateBoard(board: preview.board)
-                                    }) {
-                                        Text("파일 복제하기")
-                                    }
-                                    Button(action: {
-//                                        selectedBoard = preview.board
-                                        showAlert = true
-                                    }) {
-                                        Text("파일 삭제하기")
-                                    }
-                                }))
+                                Button(action: {
+                                    editingBoardID = preview.id
+                                }) {
+                                    Text("이름 바꾸기")
+                                }
+                                Button(action: {
+                                    //                                        duplicateBoard(board: preview.board)
+                                }) {
+                                    Text("파일 복제하기")
+                                }
+                                Button(action: {
+                                    //                                        selectedBoard = preview.board
+                                    showAlert = true
+                                }) {
+                                    Text("파일 삭제하기")
+                                }
+                            }))
                             
                         }
                     }
@@ -83,48 +83,42 @@ struct BoardGalleryView: View {
         }
         .frame(minWidth: 800, minHeight: 600)
         .confirmationDialog("정말 삭제하시겠습니까?", isPresented: $showAlert, titleVisibility: .visible) {
-            Button("네", role: .none)
-            {   print("yes")
-                if let boardToDelete = selectedBoard {
-                    deleteBoard(board: boardToDelete)
-                }
+            Button("네", role: .none) {
+                print("yes")
+                //                if let boardToDelete = selectedBoard {
+                //                    deleteBoard(board: boardToDelete)
+                //                }
             }
-            Button("아니오", role: .cancel){}
-        }.dialogSeverity(.critical)
+            Button("아니오", role: .cancel) {}
+        }
+        .dialogSeverity(.critical)
         
-            .toolbar {
-                ToolbarItem(placement: .primaryAction) {
-                    Menu {
-                        Button(action: {
-                            self.addBoard(.emptyData)
-                        }, label: {
-                            Text("새 보드")
-                        })
-                        CSVImportButton()
-                    } label: {
-                        Image(systemName: "square.and.pencil")
-                    }
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Menu {
+                    Button(action: {
+                        newDocument { KPBoardDocument() }
+                    }, label: {
+                        Text("새 보드")
+                    })
+                    CSVImportButton()
+                } label: {
+                    Image(systemName: "square.and.pencil")
                 }
             }
-            .fileImporter(isPresented: $showFileChooser, allowedContentTypes: [.init(filenameExtension: "csv")!]) { result in
-                switch result {
-                case .success(let file):
-                    print(file)
-                    let gotAccess = file.startAccessingSecurityScopedResource()
-                    if !gotAccess { return }
-                    do {
-                        let contents = try String(contentsOf: file, encoding: .utf8)
-                        print(contents)
-                        let dictionaryData = try CSVConvertManager.csvStringToDictionary(contents)
-                        let parsedNodes = dictionaryData.map { dictionary in
-                            CSVConvertManager.dictionaryToKPNode(dictionary)
-                        }
-                        var newBoard = KPBoard.emptyData
-                        newBoard.addNodes(parsedNodes)
-                        dump("newBoard : \(newBoard)")
-                        self.addBoard(newBoard)
-                    } catch {
-                        print(error.localizedDescription)
+        }
+        .fileImporter(isPresented: $showFileChooser, allowedContentTypes: [.init(filenameExtension: "csv")!]) { result in
+            switch result {
+            case .success(let file):
+                print(file)
+                let gotAccess = file.startAccessingSecurityScopedResource()
+                if !gotAccess { return }
+                do {
+                    let contents = try String(contentsOf: file, encoding: .utf8)
+                    print(contents)
+                    let dictionaryData = try CSVConvertManager.csvStringToDictionary(contents)
+                    let parsedNodes = dictionaryData.map { dictionary in
+                        CSVConvertManager.dictionaryToKPNode(dictionary)
                     }
                     var newBoard = KPBoard.emptyData
                     newBoard.addNodes(parsedNodes)
@@ -132,9 +126,13 @@ struct BoardGalleryView: View {
                     let newBoardDocument = KPBoardDocument()
                     newBoardDocument.board = newBoard
                     newDocument { newBoardDocument }
+                    
                 } catch {
                     print(error.localizedDescription)
                 }
+                file.stopAccessingSecurityScopedResource()
+            case .failure(let error):
+                print(error.localizedDescription)
             }
         }
         .onAppear {
@@ -154,17 +152,17 @@ struct BoardGalleryView: View {
             Text("CSV에서 가져오기")
         })
     }
-
+    
     func duplicateBoard(board: KPBoard) {
-//        let newBoard = KPBoard(title: board.title, nodes: board.nodes, edges: board.edges,
-//                               texts: board.texts, modifiedDate: Date(), viewType: board.viewType)
-//        boards.append(newBoard)
+        //        let newBoard = KPBoard(title: board.title, nodes: board.nodes, edges: board.edges,
+        //                               texts: board.texts, modifiedDate: Date(), viewType: board.viewType)
+        //        boards.append(newBoard)
     }
-
+    
     func deleteBoard(board: KPBoard) {
-//        if let index = boards.firstIndex(where: { $0.id == board.id }) {
-//            boards.remove(at: index)
-//        }
+        //        if let index = boards.firstIndex(where: { $0.id == board.id }) {
+        //            boards.remove(at: index)
+        //        }
     }
 }
 
@@ -184,22 +182,22 @@ extension BoardGalleryView {
         }
         self.previews = previews.sorted { $0.modifiedDate > $1.modifiedDate }
     }
-
+    
     func documentFromFileURL(_ url: URL) throws -> KPBoardDocument {
         guard url.isFileURL else {
             throw CocoaError(.fileReadInvalidFileName)
         }
-
+        
         let data = try Data(contentsOf: url)
-
+        
         let board = try JSONDecoder().decode(KPBoard.self, from: data)
-
+        
         let document = KPBoardDocument()
         document.board = board
-
+        
         return document
     }
-
+    
     @MainActor
     private func openDocumentOnMainThread(_ url: URL) async {
         print(url)
